9237b722a03b88be860d96c451afb3c2473d2b51
Baker <dragonmost@live.ca>
2023-09-29 16:01:44 -0400

    Add Hangman

---
+++ Hangman/CSharp/hangman/Program.cs
@@ -0,0 +1,100 @@
+﻿using System.Text;
+
+Console.WriteLine("John Microsoft is a bad man and is on the hook! Can you save him.");
+
+int HP = 6;
+string Goal = RandomWord();
+StringBuilder Word = new StringBuilder("".PadRight(Goal.Length, '_'));
+List<char> UsedLetters = new();
+
+while (HP > 0)
+{
+    if(UsedLetters.Count > 0)
+    {
+        Console.WriteLine($"Used letters: {string.Join("", UsedLetters)}");
+    }
+
+    DisplayGame(HP, Word.ToString());
+
+    char inputChar = GetPlayerCharacter();
+    if (inputChar == char.MinValue)
+    {
+        continue;
+    }
+    else if (UsedLetters.Contains(inputChar))
+    {
+        Console.WriteLine($"{inputChar} already used.");
+        continue;
+    }
+
+    UsedLetters.Add(inputChar);
+    if (!Goal.Contains(inputChar))
+    {
+        HP--;
+    }
+    else
+    {
+        for (int i = 0; i < Goal.Length; i++)
+        {
+            if (Goal[i] == inputChar)
+            {
+                Word[i] = i == 0 ? char.ToUpper(inputChar) : inputChar;
+            }
+        }
+    }
+
+    if (Word.ToString().ToLower() == Goal)
+    {
+        Console.WriteLine($"You saved John! The word was {Word}.");
+        Console.WriteLine(" O\r\n/|\\\r\n/ \\");
+        return;
+    }
+}
+
+Console.WriteLine("John died!");
+DisplayGame(HP, Word.ToString());
+
+char GetPlayerCharacter()
+{
+    string input = Console.ReadLine();
+
+    if(input?.Length != 1)
+    {
+        Console.WriteLine("Invalid input length");
+        return char.MinValue;
+    }
+
+    return input.ToLower()[0];
+}
+
+void DisplayGame(int hp, string word)
+{
+    Console.WriteLine(@$"
+  +---+
+  |   |
+  {(hp <= 5 ? "O" : " ")}   |
+ {(hp <= 3 ? "/" : " ")}{(hp <= 4 ? "|" : " ")}{(hp <= 2 ? "\\" : " ")}  |
+ {(hp <= 1 ? "/" : " ")} {(hp <= 0 ? "\\" : " ")}  |
+      |
+=========");
+    Console.WriteLine(word);
+}
+
+string RandomWord()
+{
+    string[] dictionary = new string[] {"ant", "babboon", "badger", "bat", "bear", "beaver", "camel", "cat", "clam", "cobra",
+    "cougar", "coyote", "crow", "deer", "dog", "donkey", "duck", "eagle", "ferret", "fox",
+    "frog", "goat", "goose", "hawk", "lion", "lizard", "llama", "mole", "rat", "raven",
+    "rhino", "shark", "sheep", "spider", "toad", "turkey", "turtle", "wolf", "wombat", "zebra"};
+    return dictionary[new Random().Next(0, dictionary.Length)];
+}
+
+/*
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+*/
---
+++ build.ps1
@@ -0,0 +1,65 @@
+param (
+    [string]$p='hangman',
+    [string]$s='CSharp',
+    [switch]$clean
+    )
+
+function IsDirty {
+    param (
+        [string]$sourcePath,
+        [string]$exePath
+    )
+
+    if (-not [System.IO.File]::Exists($exePath)) {
+        return $true
+    }
+
+    (Get-Item $sourcePath).LastWriteTime -gt (Get-Item $exePath).LastWriteTime
+}
+
+function BuildRunHangman {
+    param (
+        [string] $source
+    )
+
+    switch ($source.ToLower())
+    {
+        {($_ -eq 'csharp') -or ($_ -eq 'c#') -or ($_ -eq 'cs')} {
+            $csPath = ($PSScriptRoot + '\Hangman\CSharp\hangman\Program.cs')
+            $csprojPath = ($PSScriptRoot + '\Hangman\CSharp\hangman\hangman.csproj')
+            $exePath = ($PSScriptRoot + '\Hangman\CSharp\hangman\bin\Debug\net7.0\hangman.exe')
+
+            if (IsDirty $csPath $exePath) {
+                dotnet build $csprojPath
+            }
+            . $exePath
+        }
+        default { Write-Host 'Unavailable source' }
+    }
+
+}
+
+function CleanHangmanFiles {
+    $buildDirectories = @(
+        ($PSScriptRoot + '\Hangman\CSharp\hangman\bin\'),
+        ($PSScriptRoot + '\Hangman\CSharp\hangman\obj\'),
+    )
+
+    foreach ($dir in $buildDirectories) {
+        Remove-Item -Path $dir -Recurse -Force -errorAction SilentlyContinue
+    }
+}
+
+function CleanBuildFiles {
+    CleanHangmanFiles
+}
+
+if ($clean) {
+    CleanBuildFiles
+    return
+}
+
+switch ($p.ToLower())
+{
+    'hangman' {BuildRunHangman $s}
+}
